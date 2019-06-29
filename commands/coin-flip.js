2c33ca09dbbeba6767219d51d526e02aade0136b
Cameron Hudson <cameron.hudsonj@gmail.com>
2019-06-28 19:20:37 -0700

    eslint rules

--- commands/coin-flip.js
+++ commands/coin-flip.js
@@ -1,6 +1,6 @@
 export const name = 'coinflip';
 export const description = 'Flips a coin. Heads or Tails.';
-export function execute(message, args) {
+export function execute(message) {
   const result = Math.floor(Math.random() * 2);
   return message.channel.send(`${result ? '**(Heads)**' : '**(Tails)**'}`);
 }
