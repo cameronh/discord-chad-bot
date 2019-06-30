23070dd5d8ba8a7bdec9d3d98ce449fef69514e2
Cameron Hudson <cameron.hudsonj@gmail.com>
2019-06-30 13:04:02 -0700

    use RichEmbed in queue message

--- commands/queue.js
+++ commands/queue.js
@@ -1,12 +1,18 @@
+import Discord from 'discord.js';
+
 export const name = 'queue';
 export const description = 'Returns the current queue of songs.';
 export function execute(message) {
   const musicQueue = message.client.musicQueue;
   if (!musicQueue.songs || musicQueue.songs.length === 0) return message.channel.send('There is nothing playing.');
-  return message.channel.send(`
-  __**Song Queue:**__
-  ${musicQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
 
-  **🎶 Now playing:** ${musicQueue.songs[0].title}
-  `);
+  const queueEmbed = new Discord.RichEmbed();
+  queueEmbed.setColor('#0099ff');
+  queueEmbed.setAuthor('Song Queue');
+  musicQueue.songs.map((song, id) => {
+    if (id !== 0) queueEmbed.addField(`**(${id}):** ${song.title}`, 'YouTube');
+  });
+  queueEmbed.addBlankField();
+  queueEmbed.addField(`**🎶 Now playing:** ${musicQueue.songs[0].title}`, 'YouTube');
+  return message.channel.send(queueEmbed);
 }
