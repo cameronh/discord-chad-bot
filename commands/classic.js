c2e04fc6416d2b57fbcaf8cf857ea13cbd234bce
Cameron Hudson <cameron.hudsonj@gmail.com>
2019-08-14 10:31:40 -0700

    removed name release

--- commands/classic.js
+++ commands/classic.js
@@ -4,10 +4,7 @@ export const name = 'classic';
 export const description = 'Shows the remaining time until WoW classic releases(Not soon enough).';
 export function execute(message) {
   const release = new Date(2019, 7, 27, 0, 0, 0);
-  const nameRelease = new Date(2019, 7, 13, 0, 0, 0);
   const diffTime = countdown(Date.now().toString(), release, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
-  const diffTimeName = countdown(Date.now().toString(), nameRelease, countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
 
-  message.channel.send(`WoW: Classic Name Reserve in: **${diffTimeName.days}** Days **${diffTimeName.hours}** Hours **${diffTimeName.minutes}** Minutes **${diffTimeName.seconds}** Seconds.`);
   message.channel.send(`WoW: Classic Releases in: **${diffTime.days}** Days **${diffTime.hours}** Hours **${diffTime.minutes}** Minutes **${diffTime.seconds}** Seconds.`);
 }
